a409277f44cfd805e007c3fe24ffd362728b8b9d
James Lawler <contact@jameslawler.com>
2015-04-07 22:50:27 +0200

    Update mirror script to reload miniDLNA service

--- modules/scripts/files/mirror/mirror.sh
+++ modules/scripts/files/mirror/mirror.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Copy files
 sudo mv /media/HDD/Mirror/Movies/* /media/HDD/Media/Movies
 sudo mv /media/HDD/Mirror/TV/* /media/HDD/Media/TV
+
+# Restart miniDLNA service
+sudo mount -o remount,rw /
+sudo service minidlna force-reload
+sudo mount -o remount,rw /
